d02ed3d531e741aaba499f2060a35f3471c66e1e
Oyelowo <31687368+Oyelowo@users.noreply.github.com>
2018-05-03 18:20:05 +0300

    starting

---
+++ jsx_tutorial/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+    <div name={1+1}>Hello</div>
+    );
+  }
+}
+
+export default App;
