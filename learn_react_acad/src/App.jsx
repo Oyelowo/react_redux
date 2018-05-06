88ac79bf40aa19ce23836e48fbc3f8551fae0672
Oyelowo <31687368+Oyelowo@users.noreply.github.com>
2018-05-06 07:07:59 +0300

    some notes

--- learn_react_acad/src/App.jsx
+++ learn_react_acad/src/App.jsx
@@ -8,11 +8,19 @@ class App extends Component {
       {name: 'Oyelowo', age: 23},
       {name: 'Aleksi', age: 57},
       {name: 'Sam', age: 19}
-    ]
+    ],
+    otherState: 'some other values'
   };
 
-  switchNameHandler = () => {
-    console.log('was clicked!');
+  switchNameHandler = (newName) => {
+    // console.log('was clicked!');
+  //  DON'T DO THIS(MUTATE STATE) this.state.persons[0].name = 'BaBaNe'
+  this.setState({persons:[
+    {name: newName, age: 24}, 
+    {name: 'bright', age: 57},
+    {name: 'Sam', age: 19}
+  ]
+  })
   }
   
 
@@ -21,10 +29,17 @@ class App extends Component {
       <div className="App">
         <h1>I am a good guy </h1> 
         <p>i love this!</p>
-        <button onClick={}>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>MY interest:  Coding</Person>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+        <button onClick={this.switchNameHandler.bind(this, 'Perfect GentleMan')}>Switch Name</button>
+        <Person 
+          name={this.state.persons[0].name} 
+          age={this.state.persons[0].age}/>
+        <Person 
+        name={this.state.persons[1].name} 
+        age={this.state.persons[1].age}
+        click={this.switchNameHandler.bind(this, 'Perfe!!!')} >MY interest:  Coding</Person>
+        <Person 
+        name={this.state.persons[2].name} 
+        age={this.state.persons[2].age}/>
       </div>
     );
     // above jsx gets compiled into the javascript below:
