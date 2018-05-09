7e713cd21e0d9a782a98c2157fe9432266867ac8
Oyelowo <31687368+Oyelowo@users.noreply.github.com>
2018-05-09 16:15:17 +0300

    react and redux

--- react_redux_tm/src/actions/postActions.jsx
+++ react_redux_tm/src/actions/postActions.jsx
@@ -8,4 +8,15 @@ export const fetchPosts = () => dispatch => {
                 type: FETCH_POSTS,
                 payload: posts
             }));
-}
+};
+
+
+export const createPosts = () => dispatch => {
+    console.log('fetching');
+        fetch('https://jsonplaceholder.typicode.com/posts')
+            .then(res => res.json())
+            .then(posts => dispatch({
+                type: FETCH_POSTS,
+                payload: posts
+            }));
+};
