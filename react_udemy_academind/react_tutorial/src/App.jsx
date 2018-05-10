c7f28da56c0a7d29e798fe24bb3886b7e1198744
Oyelowo <31687368+Oyelowo@users.noreply.github.com>
2018-05-10 21:36:35 +0300

    more examples

---
+++ react_udemy_academind/react_tutorial/src/App.jsx
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import './App.css';
+import Person from './Person/Person'
+import person from './Person/Person';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>madman jaga</h1>
+        <Person/>
+      </div>
+    );
+  }
+}
+
+export default App;
