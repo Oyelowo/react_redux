6df095f1571fcea86b32d2a72614b0105dfeae4f
Oyelowo <31687368+Oyelowo@users.noreply.github.com>
2018-05-16 19:06:53 +0300

    forwarded ref

--- react_udemy_academind/chapter2/tutorial2/src/components/Persons/Person/Person.jsx
+++ react_udemy_academind/chapter2/tutorial2/src/components/Persons/Person/Person.jsx
@@ -11,12 +11,20 @@ class Person extends Component {
         super(props);
 
         console.log('[Person.js] inside constructor', props);
+        this.inputElement = React.createRef();
     }
     componentWillMount() {
         console.log('[Person.js] inside componentWillMount()');
     }
     componentDidMount() {
-        console.log('[Person.js] inside componentDidMount()')
+        console.log('[Person.js] inside componentDidMount()');
+        if (this.props.position === 0) {
+            this.inputElement.current.focus();
+        }
+    }
+
+    focus(){
+        this.inputElement.current.focus();
     }
 
     render() {
@@ -28,7 +36,9 @@ class Person extends Component {
                     years old{this.props.children}.
                     <button onClick={this.props.click}>Delete Me</button>
                 </p>
+                {/* ref are onl available in state componenets */}
                 <input
+                    ref={this.inputElement}
                     type="text"
                     value={this.props.name}
                     onChange={this.props.nameChangeEvent}/>
@@ -43,13 +53,11 @@ class Person extends Component {
 // Me</button></p>         <input type="text" value={props.name}
 // onChange={props.nameChangeEvent}/>     </div>     ) };
 
-
-// NOTE: proptypes does not work in functional component
-// it should not necessarily be used everywhere e.g when we only need
-// to display the values. but when we need to do some manipulations
-// such as calculation etc, it might be necessary and the state component should
-// be used in that case
-Person.PropTypes = {
+// NOTE: proptypes does not work in functional component it should not
+// necessarily be used everywhere e.g when we only need to display the values.
+// but when we need to do some manipulations such as calculation etc, it might
+// be necessary and the state component should be used in that case
+Person.propTypes = {
     name: PropTypes.string,
     click: PropTypes.func,
     age: PropTypes.number,
--- react_udemy_academind/chapter2/tutorial2/src/hoc/withClass.jsx
+++ react_udemy_academind/chapter2/tutorial2/src/hoc/withClass.jsx
@@ -3,21 +3,27 @@ import React, {Component} from 'react';
 // class format of the functional below. here, u dont need 
 // a class name as it is anonymous. also, props should come with this(i.e this.props)
 const withClass = (WrappedComponent, className) => {
-  return class extends Component {
+  const WithClass = class extends Component {
     render() {
       return (
         <div className={className}>
           {/* dont manipulate the wrapped component */}
-          <WrappedComponent {...this.props}/>
+          <WrappedComponent ref={this.props.forwardedRef} {...this.props}/>
         </div>
       )
     }
   }
+
+  return React.forwardRef((props, ref) =>{
+    return <WithClass {...props} forwardedRef={ref} />
+  })
 }
+
+
 
 // javascript function returning a functional Component
 // const withClass = (WrappedComponent, className) => {   return (props) => (
 //  <div className={className}>     {/* dont manipulate the wrapped component
 // */}       <WrappedComponent {...props} />     </div>   ) }
 
-export default withClass
+export default withClass;
