a32e9b84a7a3aeeefce68a9ed39ae49ead8134a7
Oyelowo <31687368+Oyelowo@users.noreply.github.com>
2018-05-25 11:35:19 +0300

    request and response interceptors

--- react_udemy_academind/http_ajax/http--01-starting-setup/src/containers/Blog/Blog.jsx
+++ react_udemy_academind/http_ajax/http--01-starting-setup/src/containers/Blog/Blog.jsx
@@ -1,64 +1,69 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 
-import Post from '../../components/Post/Post';
-import FullPost from '../../components/FullPost/FullPost';
-import NewPost from '../../components/NewPost/NewPost';
-import './Blog.css';
-import axios from 'axios';
+import Post from "../../components/Post/Post";
+import FullPost from "../../components/FullPost/FullPost";
+import NewPost from "../../components/NewPost/NewPost";
+import "./Blog.css";
+import axios from "axios";
 
 class Blog extends Component {
-    state = {
-        posts: [],
-        selectedPostId : null
-    }
+  state = {
+    posts: [],
+    selectedPostId: null,
+    error: false
+  };
 
-    componentDidMount() {
-        axios
-            .get('https:jsonplaceholder.typicode.com/posts')
-            .then(response => {
-                const posts = response
-                    .data
-                    .slice(0, 4);
-                const updatedPosts = posts.map(post => {
-                    return {
-                        ...post,
-                        author: 'Oyelowo'
-                    }
-                })
-                this.setState({posts: updatedPosts});
-                // console.log(response);
-            });
-    }
+  componentDidMount() {
+    axios
+      .get("https:jsonplaceholder.typicode.com/posts")
+      .then(response => {
+        const posts = response.data.slice(0, 4);
+        const updatedPosts = posts.map(post => {
+          return {
+            ...post,
+            author: "Oyelowo"
+          };
+        });
+        this.setState({ posts: updatedPosts });
+        // console.log(response);
+      })
+      .catch(error => {
+        this.setState({ error: true });
+        // console.log(error);
+      });
+  }
 
-    postSelectedHandler = (id) => {
-        this.setState({selectedPostId: id})
-    }
+  postSelectedHandler = id => {
+    this.setState({ selectedPostId: id });
+  };
 
-    render() {
-        const posts = this
-            .state
-            .posts
-            .map(post => {
-                return <Post
-                    key={post.id}
-                    title={post.title}
-                    author={post.author}
-                    clicked={() => this.postSelectedHandler(post.id)}/>
-            })
+  render() {
+    let posts = <p style={{ textAlign: "center" }}>Something Went wrong</p>;
+    if (!this.state.error) {
+      posts = this.state.posts.map(post => {
         return (
-            <div>
-                <section className="Posts">
-                    {posts}
-                </section>
-                <section>
-                    <FullPost id={this.state.selectedPostId}/>
-                </section>
-                <section>
-                    <NewPost/>
-                </section>
-            </div>
+          <Post
+            key={post.id}
+            title={post.title}
+            author={post.author}
+            clicked={() => this.postSelectedHandler(post.id)}
+          />
         );
+      });
     }
+
+    return (
+      <div>
+        <section className="Posts">{posts}</section>
+        <section>
+          <FullPost id={this.state.selectedPostId} />
+        </section>
+        <section>
+          <NewPost />
+        </section>
+      </div>
+    );
+  }
 }
 
 export default Blog;
--- react_udemy_academind/http_ajax/http--01-starting-setup/src/index.js
+++ react_udemy_academind/http_ajax/http--01-starting-setup/src/index.js
@@ -2,7 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import registerServiceWorker from './registerServiceWorker'
+import axios from 'axios';
+
+// this will affect all others because it is global here in index.js
+axios.interceptors.request.use(request =>{
+    console.log(request);
+    // you need to always return the request, otherwise, you're blocking the request config
+    //Edit request config
+    return request
+}, error=> {
+    console.log(error);
+    return Promise.reject(error);
+});
+
+axios.interceptors.response.use(response =>{
+    console.log(response);
+    // you need to always return the request, otherwise, you're blocking the request config
+    //Edit request config
+    return response
+}, error=> {
+    console.log(error);
+    return Promise.reject(error);
+})
 
 ReactDOM.render( <App />, document.getElementById( 'root' ) );
 registerServiceWorker();
