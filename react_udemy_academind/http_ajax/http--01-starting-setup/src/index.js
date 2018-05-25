4b7b1278bf00813a9cd0380d821742cfae056dd4
Oyelowo <31687368+Oyelowo@users.noreply.github.com>
2018-05-25 23:24:25 +0300

    using instances in axios

--- react_udemy_academind/http_ajax/http--01-starting-setup/src/index.js
+++ react_udemy_academind/http_ajax/http--01-starting-setup/src/index.js
@@ -5,6 +5,11 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker'
 import axios from 'axios';
 
+// setting default configurations globally using axios
+// axios.defaults.baseURL = 'https:jsonplaceholder.typicode.com';
+axios.defaults.headers.common['Authorization'] = 'AUTH TOKEN';
+axios.defaults.headers.post['Content-Type'] = 'application/json'
+
 // this will affect all others because it is global here in index.js
 axios.interceptors.request.use(request =>{
     console.log(request);
