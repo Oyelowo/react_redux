3907bf855fd8d2a1dabb92637b544ab071eb4ece
Oyelowo <31687368+Oyelowo@users.noreply.github.com>
2018-05-27 07:02:31 +0300

    app broken. new post and full post moved into blog folder

---
+++ react_udemy_academind/http_ajax/http--01-starting-setup/src/containers/Blog/Posts/Posts.jsx
@@ -0,0 +1,34 @@
+import React, {Component} from 'react';
+
+class Posts extends Component {
+    state = {
+        posts: [],
+        // selectedPostId: null,
+        // error: false
+      };
+    
+
+    render() {
+        let posts = <p style={{
+            textAlign: "center"
+        }}>Something Went wrong</p>;
+        if (!this.state.error) {
+            posts = this
+                .state
+                .posts
+                .map(post => {
+                    return (<Post
+                        key={post.id}
+                        title={post.title}
+                        author={post.author}
+                        clicked={() => this.postSelectedHandler(post.id)}/>);
+                });
+        }
+
+        return (
+            <section className="Posts">{posts}</section>
+        );
+    }
+}
+
+export default Posts;
