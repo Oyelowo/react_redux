5eaf254a132116a8f701efb249df8715dc26fa12
Oyelowo <31687368+Oyelowo@users.noreply.github.com>
2018-05-30 17:34:17 +0300

    exercise done. redirect

--- react_udemy_academind/react_redux_acad_tasks/tasks/routing_assignment/src/components/NavBar/NavBar.jsx
+++ react_udemy_academind/react_redux_acad_tasks/tasks/routing_assignment/src/components/NavBar/NavBar.jsx
@@ -1,8 +1,9 @@
 import React, {Component} from 'react';
-import {Route, NavLink, Switch} from 'react-router-dom';
+import {Route, NavLink, Switch, Redirect} from 'react-router-dom';
 import './NavBar.css';
 import Users from '../../containers/Users/Users';
 import Courses from '../../containers/Courses/Courses';
+import NoMatch from '../NoMatch/NoMatch';
 // import Course from '../../containers/Course/Course';
 
 
@@ -28,6 +29,8 @@ class NavBar extends Component {
                     <Route path="/users" component={Users} />
                     {/* <Route path="/courses/:courseId" component={Course}/> */}
                     <Route path="/courses" component={Courses}/>
+                    <Redirect from="/all-courses" to="/courses" />
+                    <Route component={NoMatch} />
                 </Switch>
 
             </div>
