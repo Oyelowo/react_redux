c3724ff6f22e3ae88ba492c3cd3547ce5c53fc8c
Oyelowo <31687368+Oyelowo@users.noreply.github.com>
2018-06-04 06:24:17 +0300

    react-redux installed. setting up store for app

--- tutorials_redux/redux-01-start/redux--01-start/redux-basics.js
+++ tutorials_redux/redux-01-start/redux--01-start/redux-basics.js
@@ -8,15 +8,40 @@ const initialState = {
 
 // Reducers
 const rootReducer = (state = initialState, action) => {
-    return state;
+    switch (action.type) {
+        case 'INC_COUNTER':
+            return {
+                ...state,
+                counter: state.counter + 1
+            };
+        case 'ADD_COUNTER':
+            return {
+                ...state,
+                counter: state.counter + action.value
+            };
+        default:
+            return state;
+    }
 };
+
+
 
 // store 
 const store = createStore(rootReducer);
 console.log(store.getState());
 
-
+// Subscription
+store.subscribe(()=> {
+    console.log('[Subscription]', store.getState());
+});
 
 // Dsispatching Action
+store.dispatch({
+    type: 'INC_COUNTER'
+});
+store.dispatch({
+    type: 'ADD_COUNTER',
+    value: 15
+});
+console.log(store.getState());
 
-// Subscription
