38de482f19c3d9e41e80aef20b83f7938c37da4a
Oyelowo <31687368+Oyelowo@users.noreply.github.com>
2018-06-04 11:21:55 +0300

    updating state immutably

--- tutorials_redux/redux-01-start/redux--01-start/src/containers/Counter/Counter.jsx
+++ tutorials_redux/redux-01-start/redux--01-start/src/containers/Counter/Counter.jsx
@@ -1,36 +1,49 @@
-import React, { Component } from 'react';
-import { connect } from "react-redux";
+import React, {Component} from 'react';
+import {connect} from "react-redux";
 import CounterControl from '../../components/CounterControl/CounterControl';
 import CounterOutput from '../../components/CounterOutput/CounterOutput';
 
 class Counter extends Component {
 
-    render () {
+    render() {
         return (
             <div>
-                <CounterOutput value={this.props.ctr} />
-                <CounterControl label="Increment" clicked={this.props.onIncrementCounter} />
-                <CounterControl label="Decrement" clicked={this.props.onDecrementCounter}  />
-                <CounterControl label="Add 5" clicked={this.props.onAdd5}  />
-                <CounterControl label="Subtract 5" clicked={this.props.onSubtract5}  />
+                <CounterOutput value={this.props.ctr}/>
+                <CounterControl label="Increment" clicked={this.props.onIncrementCounter}/>
+                <CounterControl label="Decrement" clicked={this.props.onDecrementCounter}/>
+                <CounterControl label="Add 5" clicked={this.props.onAdd5}/>
+                <CounterControl label="Subtract 5" clicked={this.props.onSubtract5}/>
+                <hr/>
+                <button onClick={this.props.onStoreResult}>Store Result</button>
+                <ul>
+                    {this
+                        .props
+                        .storedResults
+                        .map(strResult => (
+                            <li key={strResult.id} onClick={this.props.onDeleteResult}>{strResult.value}</li>
+                        ))}
+                    <li onClick={this.props.onDeleteResult}></li>
+                </ul>
             </div>
         );
     }
 }
 
-
-const mapStateToProps = state =>{
+const mapStateToProps = state => {
     return {
-        ctr: state.counter
+        ctr: state.counter, 
+        storedResults: state.results
     };
 }
 
-const mapDispatchToProps = dispatch =>{
-    return{
-        onIncrementCounter : ()=> dispatch({type: 'INCREMENT'}),
-        onDecrementCounter : ()=> dispatch({type: 'DECREMENT'}),
-        onAdd5 : ()=> dispatch({type: 'ADD_5', value: 5}),
-        onSubtract5 : ()=> dispatch({type: 'SUBTRACT_5', value: 5})
+const mapDispatchToProps = dispatch => {
+    return {
+        onIncrementCounter: () => dispatch({type: 'INCREMENT'}),
+        onDecrementCounter: () => dispatch({type: 'DECREMENT'}),
+        onAdd5: () => dispatch({type: 'ADD_5', value: 5}),
+        onSubtract5: () => dispatch({type: 'SUBTRACT_5', value: 5}),
+        onStoreResult: () => dispatch({type: 'STORE_RESULT'}),
+        onDeleteResult: () => dispatch({type: 'DELETE_RESULT'})
     };
 };
 
--- tutorials_redux/redux-01-start/redux--01-start/src/store/reducer.js
+++ tutorials_redux/redux-01-start/redux--01-start/src/store/reducer.js
@@ -2,28 +2,38 @@ const INCREMENT = 'INCREMENT';
 const DECREMENT = 'DECREMENT';
 const ADD_5 = 'ADD_5';
 const SUBTRACT_5 = 'SUBTRACT_5';
+const STORE_RESULT = 'STORE_RESULT';
 
 const initialState = {
-    counter: 0
+    counter: 0,
+    results: []
 }
 
 const reducer = (state = initialState, action) => {
     switch (action.type) {
         case INCREMENT:
-            return {
-                counter: state.counter + 1
-            }
+            const newState = Object.assign({}, state);
+            newState.counter = state.counter + 1;
+            return newState;
         case DECREMENT:
             return {
+                ...state,
                 counter: state.counter - 1
             }
         case ADD_5:
             return {
+                ...state,
                 counter: state.counter + action.value
             }
         case SUBTRACT_5:
             return {
+                ...state,
                 counter: state.counter - action.value
+            }
+        case STORE_RESULT:
+            return {
+                ...state,
+                results: state.results.concat({id: new Date(), value: state.counter})
             }
         default:
             return state;
