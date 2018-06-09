efb90ab9ac9db0480ea95cb2ad5efff33f6abedf
Oyelowo <31687368+Oyelowo@users.noreply.github.com>
2018-06-09 03:47:52 +0300

    using utility functions to update object

--- tutorials_redux/redux-01-start/redux--01-start/src/store/reducers/result.js
+++ tutorials_redux/redux-01-start/redux--01-start/src/store/reducers/result.js
@@ -1,4 +1,5 @@
 import * as actionTypes from '../actions/actionTypes';
+import {updateObject} from '../utility';
 
 const initialState = {
     results: []
@@ -8,23 +9,18 @@ const reducer = (state = initialState, action) => {
     switch (action.type) {
     
         case actionTypes.STORE_RESULT:
-            return {
-                ...state,
-                results: state.results.concat({
-                    id: new Date(),
-                    value: action.result *4
-                })
-            }
+        return updateObject(state,  {results: state.results.concat({
+            id: new Date(),
+            value: action.result *4
+        })});
+ 
         case actionTypes.DELETE_RESULT:
             // const id = 2;
             // const newArray = [...state.results];
             // newArray.splice(id, 1)
             const updatedArray = state.results.filter(result => result.id !== action.resultElementId);
-            
-            return {
-                ...state,
-                results: updatedArray
-            }
+            return updateObject(state,  {results: updatedArray});
+     
         default:
             return state;
     }
