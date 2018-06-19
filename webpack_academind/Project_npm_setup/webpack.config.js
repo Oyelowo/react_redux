4566e61853f29c801e8c7d96504ab0c623cad6ac
Oyelowo <31687368+Oyelowo@users.noreply.github.com>
2018-06-19 15:32:02 +0300

    adding files rule

--- webpack_academind/Project_npm_setup/webpack.config.js
+++ webpack_academind/Project_npm_setup/webpack.config.js
@@ -3,9 +3,21 @@ const path = require('path');
 module.exports = {
     devtool: 'cheap-module-eval-source-map',
     entry: './src/index.jsx',
-    output:{
+    output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js',
         publicPath: ''
+    },
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: ,
+                exclude: /node_modules/
+            }
+        ]
     }
 };
