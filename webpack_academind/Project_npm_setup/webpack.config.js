65a47d73513b1d5eebbbdc7ef588d2bd53a9e703
Oyelowo <31687368+Oyelowo@users.noreply.github.com>
2018-06-19 15:26:25 +0300

    webpack config

--- webpack_academind/Project_npm_setup/webpack.config.js
+++ webpack_academind/Project_npm_setup/webpack.config.js
@@ -1,4 +1,11 @@
+const path = require('path');
+
 module.exports = {
     devtool: 'cheap-module-eval-source-map',
-    entry: './src/index.jsx'
+    entry: './src/index.jsx',
+    output:{
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        publicPath: ''
+    }
 };
