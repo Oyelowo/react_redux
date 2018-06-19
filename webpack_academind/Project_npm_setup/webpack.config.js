55e7acd322af3c1d13bd485f5bc339a4aa376e0c
Oyelowo <31687368+Oyelowo@users.noreply.github.com>
2018-06-19 16:49:43 +0300

    Injecting the Script into the index.html File

--- webpack_academind/Project_npm_setup/webpack.config.js
+++ webpack_academind/Project_npm_setup/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const autoprefixer = require('autoprefixer');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
     devtool: 'cheap-module-eval-source-map',
@@ -7,6 +8,7 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js',
+        chunkFilename: '[id].js',
         publicPath: ''
     },
     resolve: {
@@ -50,6 +52,13 @@ module.exports = {
                         test: /\.(png|jpe?g|gif)$/,
                         loader: 'url-loader?Limit=8000&name=images/[name].[ext]'
                     }
+                ],
+                plugins: [
+                    new HtmlWebpackPlugin({
+                    template: __dirname + '/src/index.html',
+                    filename: 'index.html',
+                    inject: 'body'
+                })
                 ]
             }
         ]
