b60a9e28fb3b9478e3a4c170c2e760f28ad92595
Oyelowo <31687368+Oyelowo@users.noreply.github.com>
2018-06-19 18:05:50 +0300

    setting up project

---
+++ NextJS/pages/auth/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const authIndexPage = () => {
+  return (
+    <div>
+      <h1>The Auth Index Page</h1>
+
+    </div>
+  );
+}
+
+export default authIndexPage;
---
+++ NextJS/pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const indexPage = () => {
+  return (
+    <div>
+      <h1>The Main Page</h1>
+
+    </div>
+  );
+}
+
+export default indexPage;
