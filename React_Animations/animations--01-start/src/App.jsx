56dc5ebeab48b7d3d9ce69aadccc58c23db8e177
Oyelowo <31687368+Oyelowo@users.noreply.github.com>
2018-06-20 09:54:33 +0300

    transition events

--- React_Animations/animations--01-start/src/App.jsx
+++ React_Animations/animations--01-start/src/App.jsx
@@ -31,7 +31,18 @@ class App extends Component {
 
         <br/>
         <br/>
-        <Transition in={this.state.showBlock} timeout={1000} mountOnEnter unmountOnExit>
+        <Transition
+          in={this.state.showBlock}
+          timeout={1000}
+          mountOnEnter
+          unmountOnExit
+          onEnter={() => console.log('onEnter')}
+          onEntering={() => console.log('onEntering')}
+          onEntered={() => console.log('onEntered')}
+          onExit={() => console.log('onExit')}
+          onExiting={() => console.log('onExiting')}
+          onExited={() => console.log('onExited')}>
+
           {state => (
             <div
               style={{
@@ -49,8 +60,7 @@ class App extends Component {
 
         </Transition>
 
-        <Modal show={this.state.modalIsOpen} closed={this.closeModal}/> 
-        {this.state.modalIsOpen
+        <Modal show={this.state.modalIsOpen} closed={this.closeModal}/> {this.state.modalIsOpen
           ? <Backdrop show/>
           : null}
         <button className="Button" onClick={this.showModal}>Open Modal</button>
